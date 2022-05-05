230136b8c90d411c07cdd488671e829aa728e674
howardi <wakzclin@gmail.com>
2022-05-05 11:19:22 +0100

    arrayloop

---
+++ app.js
@@ -0,0 +1,21 @@
+var array = [[1, 2, 3],[4, 5, 6],7,8,9,[[10, 11, 12, [13, 4, 112]], 136, 14],];
+
+console.log(array [5][0][0])
+console.log(array [3])
+console.log(array [5][0][3][0])
+console.log(array [5][1])
+console.log(array [5][0][3][1])
+
+for(var i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
+
+let array1 = [20, 46, 90, 80, 120];
+let array2 = [2, 2, 5, 4, 2];
+
+function main(){
+let n = sizeof(array1)/sizeof(array1[0]);
+let m = sizeof(array2)/sizeof(array2[0]);
+calculate(array1, array2, n, m);
+return 0;
+}
